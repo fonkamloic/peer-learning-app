885fa03be4b790aaf5faec56b6b13351db8c96c4
fonkamloic <fonkamloic@gmail.com>
2019-12-05 22:49:14 +0100

    injected dependencies before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:peer_learning/screens/LoadingScreen.dart';
-import './Login/signup.dart';
-import './Login/signin.dart';
+import 'package:peer_learning/UI/LoadingScreen.dart';
+import 'package:peer_learning/models/AppModel.dart';
+import 'package:peer_learning/services/service_locator.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'UI/signup.dart';
+import 'UI/signin.dart';
 
-
-void main() => runApp(MyApp());
+void main() {
+  setupLocator();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-        debugShowCheckedModeBanner:false,
-        home: LoadingScreen(),
-
+      debugShowCheckedModeBanner: false,
+      home: ScopedModel<AppModel>(
+        model: AppModel(),
+        child: LoadingScreen(),
+      ),
     );
   }
 }
