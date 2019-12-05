947949bc55b301dc3291129f068640efd94a613a
fonkamloic <fonkamloic@gmail.com>
2019-12-05 22:51:15 +0100

    register services and model here

---
+++ lib/services/service_locator.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+import 'package:peer_learning/models/AppModel.dart';
+import 'package:peer_learning/models/LoginModel.dart';
+import 'package:peer_learning/models/SignUpModel.dart';
+
+GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  //Register Services
+
+  //Register Models
+  locator.registerFactory<AppModel>(() => AppModel());
+  locator.registerFactory<LoginModel>(() => LoginModel());
+  locator.registerFactory<SignUpModel>(() => SignUpModel());
+}
