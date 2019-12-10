dab64c0cb58149084e1cbe0b1817fa7d4be9fe6b
fonkamloic <fonkamloic@gmail.com>
2019-12-10 22:54:59 +0100

    update

--- lib/UI/LoadingScreen.dart
+++ lib/UI/LoadingScreen.dart
@@ -1,13 +1,7 @@
-import 'dart:io';
 import 'package:splashscreen/splashscreen.dart';
 
 import 'package:flutter/material.dart';
-import 'package:peer_learning/Login/signin.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:flutter/scheduler.dart';
-
-import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:peer_learning/UI/signin.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -33,12 +27,6 @@ class _LoadingScreenState extends State<LoadingScreen>
       setState(() {});
     });
   }
-
-//  @override
-//  void dispose() {
-//    super.dispose();
-//    controller.dispose();
-//  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,84 +56,3 @@ class _LoadingScreenState extends State<LoadingScreen>
     );
   }
 }
-
-//
-//class LoadingScreen extends StatefulWidget {
-//  @override
-//  _LoadingScreenState createState() => _LoadingScreenState();
-//}
-//
-//class _LoadingScreenState extends State<LoadingScreen>
-//    with SingleTickerProviderStateMixin {
-//  AnimationController controller;
-//  Animation animation;
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//
-//    controller =
-//        AnimationController(duration: Duration(seconds: 1), vsync: this);
-//    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
-//        .animate(controller);
-//    controller.forward();
-//
-//    controller.addListener(() {
-//      setState(() {});
-//      print(animation.value);
-//    });
-//  }
-//
-////  @override
-////  void dispose() {
-////    super.dispose();
-////    controller.dispose();
-////  }
-//
-////  goToLogin() {
-////    setState(() {
-////      Navigator.push(
-////          context, MaterialPageRoute(builder: (context) => SigninPage()));
-////    });
-////  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    SchedulerBinding.instance.addPostFrameCallback((_) => setState(() {
-//          sleep(Duration(seconds: 10));
-//          Navigator.pushReplacement(
-//              context, MaterialPageRoute(builder: (context) => SigninPage()));
-//        }));
-//
-//    return Scaffold(
-//      backgroundColor: animation.value,
-//      body: Padding(
-//        padding: EdgeInsets.symmetric(horizontal: 24.0),
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          crossAxisAlignment: CrossAxisAlignment.stretch,
-//          children: <Widget>[
-//            Row(
-//              children: <Widget>[
-////                Hero(
-////                  tag: "logo",
-////                  child: Container(
-////                    child: Image.asset('images/logo.png'),
-////                    height: 60.0 * controller.value,
-////                  ),
-////                ),
-//                TypewriterAnimatedTextKit(
-//                  text: ['Peer Learning'],
-//                  textStyle: TextStyle(
-//                    fontSize: 45.0 * controller.value,
-//                    fontWeight: FontWeight.w900,
-//                  ),
-//                ),
-//              ],
-//            ),
-//          ],
-//        ),
-//      ),
-//    );
-//  }
-//}
