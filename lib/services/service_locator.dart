e562db652b473661101579a85b3208b56e4abca6
fonkamloic <fonkamloic@gmail.com>
2019-12-30 05:48:56 +0100

    registered chatModel

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:peer_learning/models/AppModel.dart';
 import 'package:peer_learning/models/LoginModel.dart';
 import 'package:peer_learning/models/SignUpModel.dart';
+import 'package:peer_learning/models/chat_model.dart';
 
 GetIt locator = GetIt.instance;
 
@@ -12,4 +13,5 @@ void setupLocator() {
   locator.registerFactory<AppModel>(() => AppModel());
   locator.registerFactory<LoginModel>(() => LoginModel());
   locator.registerFactory<SignUpModel>(() => SignUpModel());
+  locator.registerFactory<ChatModel>(() => ChatModel());
 }
